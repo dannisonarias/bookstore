345c1f841b58f7f70ae3486a0e094a840c7a40d6
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-16 16:52:46 -0300

    Added basic form

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+const BOOKS_CATEGORY = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"];
+
 const BooksForm = () => {
-    return (<h1>BooksForm</h1>)
+  return (
+    <form>
+      <label>Title: </label>
+      <input type="text" name="title" />
+      <label>Category: </label>
+      <select name="Category"></select>
+      <input type="submit"/>
+    </form>
+  );
 };
 
 export default BooksForm;

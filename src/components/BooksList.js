6cd95addb7178617576e6c98d33ec2f48720b5fc
DannisonArias <dannisonarias@gmail.com>
2020-07-16 18:14:28 -0400

    fix booklist structure

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Book from './Book';
 
-const mapStateToProps = (state) => {
-    return state;
-};
+const mapStateToProps = state => state;
 
 const handleRemove = () => {
 };
 
-const BookList = (props) => {
-    const {title, category } = props.bookReducer[145];
-    return (
+const BookList = props => {
+  const { title, category } = props.bookReducer[145];
+  return (
     <table>
-        <Book id='1' title={title} category={category} />
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Book id="1" title={title} category={category} />
+      </tbody>
     </table>
-    )
+  );
 };
 
 export default connect(mapStateToProps)(BookList);

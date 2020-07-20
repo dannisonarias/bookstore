862b91eb91af8fbcc2fb3850a2942119c8486043
DannisonArias <dannisonarias@gmail.com>
2020-07-20 17:18:49 -0400

    add default category

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,16 +10,20 @@ const Book = props => {
     <tr>
       <th>{id}</th>
       <th>{title}</th>
-      <th>{category}</th>
+      <th>{(category === '' ? 'None' : category)}</th>
       <th><button type="button" onClick={() => { handleRemove(id); }}>remove</button></th>
     </tr>
   );
 };
 
+Book.defaultProps = {
+  category: 'None',
+};
+
 Book.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
+  category: PropTypes.string,
   handleRemove: PropTypes.func.isRequired,
 };
 

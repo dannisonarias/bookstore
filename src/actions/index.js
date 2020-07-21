79deb83ad7525204c7d57a439353ccab2b45fbf3
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-21 11:41:18 -0300

    Added containers directory. Moved booklist and bookform there. Change export method on first action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-export const REMOVE_BOOK = book => ({ type: 'REMOVE_BOOK', book });
+const REMOVE_BOOK = book => ({ type: 'REMOVE_BOOK', book });
 const ADD_BOOK = book => ({ type: 'ADD_BOOK', book });
 
-export default ADD_BOOK;
+export { ADD_BOOK, REMOVE_BOOK };
